f98d403a43631a1071589380e5dcb8332a9e99bf
DTDUONG98 <dotienduongptit@gmail.com>
2020-09-29 11:14:20 +0700

    Updates

---
+++ precache-manifest.f7f622d0a3c7832b3702c2618f11e45c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b94d0b5c28b148ba8b01887865f1e6",
+    "url": "/testheroku/index.html"
+  },
+  {
+    "revision": "2ec76b71ec38b8d2de30",
+    "url": "/testheroku/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d4c2154842b4c8dab80",
+    "url": "/testheroku/static/js/2.4bbfc983.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testheroku/static/js/2.4bbfc983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec76b71ec38b8d2de30",
+    "url": "/testheroku/static/js/main.c9db6842.chunk.js"
+  },
+  {
+    "revision": "62adfc8742e35124ee8b",
+    "url": "/testheroku/static/js/runtime-main.64499d94.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testheroku/static/media/logo.5d5d9eef.svg"
+  }
+]);
